fa732eae41461b4b638e2a6f8195ffbbd8618624
RAKOTONDARANONY Noah Mbolarivomanana <noahrakoto342@gmail.com>
2026-06-17 09:35:21 +0300

    change
    ez

--- codeigniter4-framework-b3359be/app/Controllers/CaisseController.php
+++ codeigniter4-framework-b3359be/app/Controllers/CaisseController.php
@@ -1,11 +1,16 @@
 <?php 
 
+namespace App\Controllers; // 1. On indique où se trouve ce fichier
+
+use App\Controllers\BaseController; // 2. On importe le BaseController de CodeIgniter 4
+
 class CaisseController extends BaseController
 {
     public function index()
     {
-        return view('caisse');
+        return view('caisse/caisse');
     }
+
     public function search()
     {
         $searchTerm = $this->request->getGet('search');
